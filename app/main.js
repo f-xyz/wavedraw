6b2a91a9b78306d5645133fc073aafb3de29489e
andr <andrey.lischina@gmail.com>
2013-10-03 03:01:28 +0300

    3.10.13

--- app/main.js
+++ app/main.js
@@ -1,24 +1,35 @@
-console.log('init');
-
 require.config({
     baseUrl: 'app',
     paths: {
-        jquery:     'lib/jquery/jquery.min',
-        stats:      'lib/stats.js/build/stats.min',
-        underscore: 'lib/underscore/underscore-min'
+        jquery:         '../lib/jquery/jquery.min',
+        stats:          '../lib/stats.js/build/stats.min',
+        es5shim:        '../lib/es5-shim/es5-shim.min',
+        es5sham:        '../lib/es5-shim/es5-sham.min',
+        angular:        '../lib/angular/angular.min'
     },
-//    shim: {
-//        jquery:       { exports: "$" },
-//        underscore:   { exports: '_' },
-//        stats:        { exports: 'Stats' }
-//    },
+    shim: {
+        jquery:       { exports: '$' },
+        stats:        { exports: 'Stats' }
+    },
 //    packages: ['ge'],
     urlArgs: Date.now()
 });
 
-require(['viewport'], function(Viewport) {
-    console.log('require');
+require(
+    ['utils', 'es5shim', 'es5sham', 'stats', 'angular',
+     'modules/sandbox', 'modules/viewport', 'modules/world'],
+    function() {
+        'use strict';
 
-    var canvas = document.querySelector('canvas');
-    var viewport = new Viewport(canvas);
-});
+        var canvas = document.querySelector('canvas');
+        var viewport = require('modules/viewport');
+        viewport.initialize(canvas);
+
+        var sandbox = require('modules/sandbox');
+        sandbox.onAny(function(e) {
+            if (e.__event !== 'tick') {
+                console.log(e);
+            }
+        });
+    }
+);
