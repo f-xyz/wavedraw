6e0afae0a97e84ce289384bbe4b58396062ce0e8
andr <andrey.lischina@gmail.com>
2013-10-14 23:48:30 +0300

    .

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req, res) {
-    console.log(req);
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('test');
 }).listen(10000);
